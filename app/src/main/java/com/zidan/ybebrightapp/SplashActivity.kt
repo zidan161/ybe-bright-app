4566408a698e58ae6424d874c9c5f60c66b07d89
zidan161 <zidanam161@gmail.com>
2022-05-17 05:01:53 +0700

    add Auth Firebase

--- app/src/main/java/com/zidan/ybebrightapp/SplashActivity.kt
+++ app/src/main/java/com/zidan/ybebrightapp/SplashActivity.kt
@@ -1,17 +1,43 @@
 package com.zidan.ybebrightapp
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.zidan.ybebrightapp.login.LoginActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity: AppCompatActivity() {
+
+    companion object {
+        const val EMAIL = "ybebright@mail.com"
+        const val PASS = "ybebrightdbaccess"
+    }
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.SplashTheme)
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
+        auth = Firebase.auth
+        auth.signInWithEmailAndPassword(EMAIL, PASS).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
